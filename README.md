feade0f61f0dff44a8dcebd5bcd2c51465596af7
chadd williams <will4614@pacificu.edu>
2022-10-23 21:25:21 -0700

    make all number floats

---
+++ README.md
@@ -0,0 +1,10 @@
+Collatz in Python!
+
+Loop while not 1
+
+	If the number is even
+		divide by 2
+
+	If the number is odd
+		multiply by 3 and add 1
+
--- simpleLoop.py
+++ simpleLoop.py
@@ -14,13 +14,12 @@ steps = 0
 # loop until we hit 1
 while 1 != n:
 
-
   if 0 == n % 2:
     # even
     n = n / 2
   else:
     # odd
-    n = n * 3 + 1
+    n = n * 3 + 1.0
 
   # update the user on our progress
   print("Current value: " + str(n))
